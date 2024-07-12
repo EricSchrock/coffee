284487dd22b72e9340cb9b8bc129916f15d94a9f
Eric Schrock <eric.schrock22@gmail.com>
2024-07-12 16:40:28 -0400

    Tune coffee regex

--- src/regex.py
+++ src/regex.py
@@ -1,4 +1,4 @@
 is_new_york = r"(?i).*\b(?:new york|new yorkers?|N\.?Y\.?C?\.?)\b.*"
 is_1900_to_1909 = r"^190[0-9].*"
 is_dollars = r"(?i)^dollars$"
-is_cup_of_coffee = r"(?i)(?:^(?=.*\bcoffee\b)(?=.*\bcup\b).*)|(?:^coffee$)"
+is_cup_of_coffee = r"(?i)(?:^coffee$)|(?:^black coffee$)|(?:^(?=.*\bcoffee\b)(?=.*\b(?:cup|mug|glass|demi[- ]tasse|goblet)\b)(?!.*\b(?:pot|pitcher|jug|pound|lb|machine|for two|all you)\b)(?!.*\b(?:iced?|cream|milk|cocktail)\b).*)"
