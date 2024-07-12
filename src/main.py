0097b5f86af189a2d1aca00fcbc1cc5c11b214ff
Eric Schrock <eric.schrock22@gmail.com>
2024-07-12 14:30:19 -0400

    Remove leading and trailing whitespace

--- src/main.py
+++ src/main.py
@@ -11,30 +11,38 @@ from typing import Callable, List, Tuple
 from regex import is_new_york, is_1900_to_1909, is_dollars
 
 
-def repair_menu_date_from_call_number(menu_df, page_df, item_df, dish_df) -> None:
+def remove_leading_and_trailing_whitespace(menu_df: pd.DataFrame, page_df: pd.DataFrame, item_df: pd.DataFrame, dish_df: pd.DataFrame) -> None:
+    menu_df['date'] = menu_df['date'].apply(lambda x: x.strip() if type(x) == str else x)
+    menu_df['call_number'] = menu_df['call_number'].apply(lambda x: x.strip() if type(x) == str else x)
+    menu_df['place'] = menu_df['place'].apply(lambda x: x.strip() if type(x) == str else x)
+    menu_df['currency'] = menu_df['currency'].apply(lambda x: x.strip() if type(x) == str else x)
+    item_df['price'] = item_df['price'].apply(lambda x: x.strip() if type(x) == str else x)
+    dish_df['name'] = dish_df['name'].apply(lambda x: x.strip() if type(x) == str else x)
+
+def repair_menu_date_from_call_number(menu_df: pd.DataFrame, page_df: pd.DataFrame, item_df: pd.DataFrame, dish_df: pd.DataFrame) -> None:
     regex = re.compile(r"^[0-9][0-9][0-9][0-9]-.*")
     menu_df.loc[menu_df['date'].isna() & menu_df['call_number'].str.contains(regex, na=False), 'date'] = menu_df['call_number'].str[:4]
 
-def repair_menu_date_outside_expected_range(menu_df, page_df, item_df, dish_df) -> None:
+def repair_menu_date_outside_expected_range(menu_df: pd.DataFrame, page_df: pd.DataFrame, item_df: pd.DataFrame, dish_df: pd.DataFrame) -> None:
     menu_df['date'] = menu_df['date'].str.replace(r"^0190", "1900", regex=True) # Typos observed in manual data exploration
     menu_df['date'] = menu_df['date'].str.replace(r"^1091", "1901", regex=True)
     menu_df['date'] = menu_df['date'].str.replace(r"^2928", "1928", regex=True)
 
-def repair_menu_place_new_york_spelling(menu_df, page_df, item_df, dish_df) -> None:
+def repair_menu_place_new_york_spelling(menu_df: pd.DataFrame, page_df: pd.DataFrame, item_df: pd.DataFrame, dish_df: pd.DataFrame) -> None:
     regex = re.compile(r"enw york|nwe york|ne wyork|newy ork|new yrok|new yokr|\bew york|nw york|ne york|newyork|new ork|new yrk|new yok|new yor\b|[^n]ew york|n[^e]w york|ne[^w] york|new[^ ]york|new [^y]ork|new y[^o]rk|new yo[^r]k|new yor[^k]", re.IGNORECASE)
     menu_df['place'] = menu_df['place'].str.replace(regex, 'NEW YORK', regex=True)
 
-def repair_menu_currency_dollars_spelling(menu_df, page_df, item_df, dish_df) -> None:
+def repair_menu_currency_dollars_spelling(menu_df: pd.DataFrame, page_df: pd.DataFrame, item_df: pd.DataFrame, dish_df: pd.DataFrame) -> None:
     regex = re.compile(r"^(?:odllars|dlolars|dolalrs|dollras|dollasr|ollars|dllars|dolars|dollrs|dollas|dollar|[^d]ollars|d[^o]llars|do[^l]lars|dol[^l]ars|doll[^a]rs|dolla[^r]s|dollar[^s])$", re.IGNORECASE)
     menu_df['currency'] = menu_df['currency'].str.replace(regex, 'Dollars', regex=True)
 
-def repair_menu_currency_convert_cents_to_dollars(menu_df, page_df, item_df, dish_df) -> None:
+def repair_menu_currency_convert_cents_to_dollars(menu_df: pd.DataFrame, page_df: pd.DataFrame, item_df: pd.DataFrame, dish_df: pd.DataFrame) -> None:
     menu_ids = menu_df.loc[menu_df['currency'] == 'Cents', 'id']
     menu_df.loc[menu_df['currency'] == 'Cents', 'currency'] = "Dollars"
     page_ids = page_df.loc[page_df['menu_id'].isin(menu_ids), 'id']
     item_df.loc[item_df['menu_page_id'].isin(page_ids) & item_df['price'].notnull(), 'price'] /= 100
 
-def repair_dish_name_coffee_spelling(menu_df, page_df, item_df, dish_df) -> None:
+def repair_dish_name_coffee_spelling(menu_df: pd.DataFrame, page_df: pd.DataFrame, item_df: pd.DataFrame, dish_df: pd.DataFrame) -> None:
     regex = re.compile(r"\b(?:ocffee|cfofee|cofefe|offee|cffee|cofee|coffe|[^ct]offee|c[^o]ffee|co[^f]fee|cof[^f]ee|coff[^e]e|coffe[^e])\b", re.IGNORECASE)
     dish_df['name'] = dish_df['name'].str.replace(regex, 'Coffee', regex=True)
 
@@ -79,6 +87,7 @@ def profile_dish_data(dish_df: pd.DataFrame) -> int:
 @timer
 def clean_data(menu_df: pd.DataFrame, page_df: pd.DataFrame, item_df: pd.DataFrame, dish_df: pd.DataFrame) -> None:
     cleaning_routines = [
+        remove_leading_and_trailing_whitespace,
         repair_menu_date_from_call_number,
         repair_menu_date_outside_expected_range,
         repair_menu_place_new_york_spelling,
