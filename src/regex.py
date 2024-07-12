180092002e1a0d1941eab797d972f589e57fa33b
Eric Schrock <eric.schrock22@gmail.com>
2024-07-12 16:02:02 -0400

    Broader coffee regex

--- src/regex.py
+++ src/regex.py
@@ -1,4 +1,4 @@
 is_new_york = r"(?i).*\b(?:new york|new yorkers?|N\.?Y\.?C?\.?)\b.*"
 is_1900_to_1909 = r"^190[0-9].*"
-is_dollars = r"^Dollars$"
-is_cup_of_coffee = r"^(?=.*\bcoffee\b)(?=.*\bcup\b).*"
+is_dollars = r"(?i)^dollars$"
+is_cup_of_coffee = r"(?i)(?:^(?=.*\bcoffee\b)(?=.*\bcup\b).*)|(?:^coffee$)"
