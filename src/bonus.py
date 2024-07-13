67e28ff5d342ab54fb10440bc051fbea9741f415
Eric Schrock <eric.schrock22@gmail.com>
2024-07-13 17:03:21 -0400

    Simplify and broaden bonus.py

--- src/bonus.py
+++ src/bonus.py
@@ -1,4 +1,3 @@
-import re
 import sqlite3
 from argparse import ArgumentParser
 from statistics import mean, median
@@ -9,19 +8,13 @@ import pandas as pd
 import seaborn as sns
 
 from main import clean_data, load_data, timer
-from regex import IS_CUP_OF_COFFEE, IS_NEW_YORK
 
 
 @timer
 def query_data(menu_df: pd.DataFrame, page_df: pd.DataFrame, item_df: pd.DataFrame, dish_df: pd.DataFrame) -> List[Tuple[int, float, float]]:
     menu_df['year'] = pd.to_datetime(menu_df.date, errors='coerce').dt.year
 
-    def regexp(expr, item):
-        regex = re.compile(expr, re.IGNORECASE)
-        return bool(regex.search(item))
-
     con = sqlite3.connect(":memory:")
-    con.create_function("REGEXP", 2, regexp)
     cur = con.cursor()
 
     menu_df.to_sql("Menu", con, if_exists='replace', index=False, method='multi', chunksize=10_000)
@@ -34,17 +27,17 @@ def query_data(menu_df: pd.DataFrame, page_df: pd.DataFrame, item_df: pd.DataFra
         INNER JOIN dish ON item.dish_id = dish.id
         INNER JOIN page ON item.menu_page_id = page.id
         INNER JOIN menu ON page.menu_id = menu.id
-        WHERE dish.name IS NOT NULL AND dish.name REGEXP ?
-        AND menu.place IS NOT NULL AND menu.place REGEXP ?
+        WHERE dish.name LIKE "%coffee%"
+        AND menu.year IS NOT NULL
         AND menu.currency = "Dollars"
-        AND item.price IS NOT NULL AND item.price < 1;
-        """, [IS_CUP_OF_COFFEE, IS_NEW_YORK]).fetchall()
+        AND item.price IS NOT NULL;
+        """).fetchall()
 
     con.close()
 
     results = [ (int(year), price) for year, price in results ]
-    years = sorted(set([ year for year, _ in results]))
-    results = [ (year, mean([ p for y, p in results if y == year]), median([ p for y, p in results if y == year])) for year in years ]
+    years = sorted({ year for year, _ in results })
+    results = [ (year, mean([ p for y, p in results if y == year ]), median([ p for y, p in results if y == year ])) for year in years ]
 
     return results
 
@@ -57,7 +50,7 @@ def save_query_results(results: List[Tuple[int, float, float]]) -> None:
     sns.set_theme()
     plt.plot(x, y_mean, label="Mean")
     plt.plot(x, y_median, label="Median")
-    plt.title("Price of a Cup of Coffee in New York")
+    plt.title("Price of Coffee")
     plt.xlabel("Year")
     plt.ylabel("Price ($)")
     plt.legend()
